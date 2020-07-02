b5128dabec2ac3c0a07110c5c4819c74c5af045a
Elastinex <elastinex@gmail.com>
2020-07-02 17:34:51 +0800

    5

--- drug/settings/common.py
+++ drug/settings/common.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
