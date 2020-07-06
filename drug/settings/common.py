bc2cce3497c32599b0263c5003d84ab04699a655
Elastinex <elastinex@gmail.com>
2020-07-06 11:55:40 +0800

    media

--- drug/settings/common.py
+++ drug/settings/common.py
@@ -153,7 +153,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
